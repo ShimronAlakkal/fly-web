30749d5e62ba534f62edf90d112bac26ac9a31e0
SharoonRafeek <sharoonrafeek@gmail.com>
2023-05-20 20:19:58 +0530

    minor bug fixes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("./public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
